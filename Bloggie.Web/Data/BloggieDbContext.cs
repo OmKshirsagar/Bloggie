c3ffa6f5223b85b7c117ca1c9d46f205f6e35bc6
OmKshirsagar <kshirsagar.om124@gmail.com>
2023-10-29 22:49:09 +0530

    Added DB Context and inital Migrations ran.

---
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public BloggieDbContext(DbContextOptions options) : base(options) {}
+    }
+}
--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -1,7 +1,15 @@
+using Bloggie.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BloggieDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("BloggieDbConnString")
+    )
+);
 
 var app = builder.Build();
 
